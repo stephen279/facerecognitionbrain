4141ea04c79549779f13a0d77765732bdfd9b436
stephen holland <stephenholland279@gmail.com>
2021-08-20 00:41:46 +0100

    seeion issues

--- src/App.js
+++ src/App.js
@@ -402,11 +402,9 @@ class App extends Component {
 			fetch('https://protected-gorge-67490.herokuapp.com/')
 			
 				.then(response => response.statusText)
-				//.getResponseHeader("Set-Cookie")
-				//.ResponseHeaders
 				
 				.then(function (data) {
-					console.log('Request response succeeded with JSON response', data);
+					console.log('Request succeeded with JSON response', data);
 					if (data != "Bad Request") {
 						console.log("got result inside checksession"+data);
 						//	this.state.route === 'home';
