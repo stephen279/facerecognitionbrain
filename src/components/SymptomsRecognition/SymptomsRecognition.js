8a6a947b8b687989ff785d5ea18f36a51e80ad83
stephen holland <stephenholland279@gmail.com>
2021-10-03 19:30:29 +0100

    adding tab icon name

--- src/components/SymptomsRecognition/SymptomsRecognition.js
+++ src/components/SymptomsRecognition/SymptomsRecognition.js
@@ -11,12 +11,12 @@ const SymptomsRecognition = () => {
         <article id = "mycard" class="center mw5 mw6-ns br3 hidden ba b--black-10 mv4">
   <h1 id = "symptoms_confidence" class="f1 bg-near-white br2 br--top black-60 mv0 pv1 ph3" ></h1>
   <div class="pa2 bt b--black-10">
-    <p id = "symptoms_result"  class="f6 f5-ns lh-copy measure"> Result will show here.
+    <p id = "symptoms_result"  class="f6 f5-ns lh-copy measure"> Result will show here and who can treat.
              
              
     </p>
             
-                <p> Treated By </p>
+               
              <p id = "specialist"  class="f6 f5-ns lh-copy measure"> 
     
       
