d5333d5deccae3d887caab058cd67d677fc1a402
stephen holland <stephenholland279@gmail.com>
2022-02-20 21:00:24 +0000

    testing imasge recognistion

--- src/App.js
+++ src/App.js
@@ -925,7 +925,7 @@ axios(config)
 
 			let confidence_new = (Math.round(res.data.confidence * 100) / 100).toFixed(2) * 100 + "%";
 			// alert(res);
-			document.getElementById('result').innerHTML = res.data.name;
+			document.getElementById('result').innerHTML = res.data.labelName;
 			document.getElementById('confidence').innerHTML = confidence_new;
 		});
 
