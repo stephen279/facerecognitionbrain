a88faeba5286ee2da3e710a5e1108b5f79fa7678
stephen holland <stephenholland279@gmail.com>
2022-02-22 21:49:40 +0000

    testing imasge recognistion

--- src/App.js
+++ src/App.js
@@ -675,7 +675,7 @@ var options = {
 
 	hshandleFormSubmit = (event) => {
 
-			//this.showOperation();
+			this.showOperation();
 		
 		
 		const age = this.state.hs_age;
@@ -911,7 +911,7 @@ axios(config)
 
 	fileUploadHandler = () => {
 		
-	 //this.showOperation
+	 this.showOperation
 		console.log("inside file upload" + this.state.selectedFile);
 		const formData = new FormData();
 		formData.append('data', this.state.selectedFile, this.state.selectedFile.name);
