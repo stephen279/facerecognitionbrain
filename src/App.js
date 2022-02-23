ca87e4eb6e90a5ab5695ef1831b8c58a8f1f04cf
stephen holland <stephenholland279@gmail.com>
2022-02-23 23:09:16 +0000

    get rid of extra zeros

--- src/App.js
+++ src/App.js
@@ -383,11 +383,11 @@ var options = {
 				console.log("fat measuremens values")
 					console.log(JSON.stringify(response.data));
 
-		//	let callString = (response.data.body.measuregrps[0].measures[0].value);
+			let callString = (response.data.body.measuregrps[0].measures[0].value);
 
-		//	let finalString = callString.slice(0, -3);
+			let finalString = callString.slice(0, -3);
 			
-		//	console.log("finalString" + finalString);
+			console.log("finalString" + finalString);
 					document.getElementById("hs_fat").value = response.data.body.measuregrps[0].measures[0].value;
 			})
 			.catch(function (error) {
